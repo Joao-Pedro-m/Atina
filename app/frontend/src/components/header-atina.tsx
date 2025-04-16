8bb5317df1eabfc4f3a2a61dac405e1e1518fec9
Marucs <marcusvnp2112@gmail.com>
2025-04-15 22:51:55 -0400

    header do estoque

---
+++ app/frontend/src/components/header-atina.tsx
@@ -0,0 +1,8 @@
+export default function HeaderAtina() {  
+  return (
+    // VERIFICAR SE TERÁ IMAGEM DO USUÁRIO 
+    <header className="bg-[#A9A9A9] w-screen text-[2.3em] font-bold static top-0 flex items-center justify-start p-[1%]">
+        <h1>ATINA ESTOQUE</h1>
+    </header>
+  );
+}
